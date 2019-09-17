98d98ec1f6911b735fd2ec930f914c5682862fc9
Simon <simon.albani@apprime.de>
2019-09-17 05:06:43 +0200

    fixed parallax

--- src/app/directives/parallax.directive.ts
+++ src/app/directives/parallax.directive.ts
@@ -30,25 +30,12 @@ export class ParallaxDirective implements AfterViewInit {
 	private setParallaxImage() {
 		//set img style
 		this.Image.style.minWidth = "100%";
+		this.Image.style.maxWidth = "150%";
 		this.Image.style.minHeight = (this.speed * 100) + "%";
-		this.Image.style.transform = "translateX(50%)";
+		this.Image.style.transform = "translateX(-50%)";
 		this.Image.style.left = "50%"
 		this.Image.style.height = "auto";
 		this.Image.style.position = "absolute";
-		this.Image.style.zIndex = "-1";
-
-		// wrap image with a div, then set style
-		var imgWrapperEl: HTMLElement = <HTMLElement>this.Container.querySelector('.parallax-img-wrapper');
-		if (!imgWrapperEl) {
-			imgWrapperEl = document.createElement('div');
-			imgWrapperEl.className = 'parallax-img-wrapper';
-			imgWrapperEl.appendChild(this.Image);
-			this.Container.appendChild(imgWrapperEl);
-		}
-
-		Object.assign(imgWrapperEl.style, {
-			position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, zIndex: -1, overflow: "hidden"
-		});
 	}
 
 	UpdateParallax(event: Event): any {
